de15057c92cfc13c10edf5d87c1ffcd3111820e0
ZhihangZhang <462143015zzh@gmail.com>
2018-10-15 19:21:17 -0700

    downgrade to react-native 0.57.1

--- front-end/mapp/App.js
+++ front-end/mapp/App.js
@@ -3,7 +3,6 @@ import {AppRegistry, Text, View, Navigator, StyleSheet} from 'react-native';
 
 import { Button } from 'react-native-paper';
 import Header from './app/components/testComponents/header';
-//import Prescription from './app/components/testComponents/prescription';
 
 type Props = {};
 export default class App extends Component<Props> {
