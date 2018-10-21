8b3ebc370a10e67a640aca4289a0a5f82674abda
ian <ianmccall93@gmail.com>
2018-10-21 00:53:52 -0700

    [server] test for multiple account creation attempts with same email

--- server/test/Handler/PostDoctorsSpec.hs
+++ server/test/Handler/PostDoctorsSpec.hs
@@ -10,6 +10,7 @@ spec = withApp $ do
   describe "valid request" $ do
     it "returns and stores the correct information after POST" $ do
 
+      -- Initial request for this email
       postJson DoctorsR $ PostDoctor
         { firstName = "James"
         , lastName = "Hill"
@@ -18,11 +19,34 @@ spec = withApp $ do
         }
       jsonResponseIs $ Entity (doctorKey 1) $ Doctor "James" "Hill" []
 
+      -- Attempt to claim this email by a second user
+      postJson DoctorsR $ PostDoctor
+        { firstName = "Jill"
+        , lastName = "Stein"
+        , email = "james@hill.com"
+        , password = "jstein"
+        }
+      statusIs 400
+
       muser <- runDB $ getBy (UserEmail "james@hill.com")
 
       case muser of
-        Nothing -> failTest "Doctor's user credentials not correctly stored after post"
+        Nothing -> failTest "No entry in User table after Doctor POST"
         (Just (Entity id user)) -> do
           assertEq "Email" (xUserEmail user) "james@hill.com"
           assertEq "Data" (xUserData user) (Left $ doctorKey 1)
+
+          case xUserData user of
+            Right _ -> failTest "Doctor incorrectly entered as patient"
+            Left did -> do
+              mdoctor <- runDB $ getEntity did
+
+              case mdoctor of
+                Nothing -> failTest "No entry in Doctor table after Doctor POST"
+                (Just (Entity _ doctor)) -> do
+                  assertEq "Doctor" doctor Doctor
+                    { doctorFirstName = "James"
+                    , doctorLastName = "Hill"
+                    , doctorPatients = []
+                    }
 
