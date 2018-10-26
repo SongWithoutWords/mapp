e13ff8b75e91a18f7ded6ecc659bd49ab65a19db
Sina Saleh <sinasaleh13@yahoo.com>
2018-10-26 15:33:12 -0700

    inbox screen removed reacted

--- front-end/mapp/app/screens/InboxScreen.js
+++ front-end/mapp/app/screens/InboxScreen.js
@@ -15,7 +15,6 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      reacted : 'blue',
       showButtons: true,
       text: 'You have a new request from'
     }
