39f115280457188d8fb7cee08aa89f523aed0fc8
amirtootooni <tootooniamirhossein@gmail.com>
2018-11-04 22:10:50 -0800

    added tests for doctor sign in

--- front-end/mapp/app/specs/DoctorListSpec.js
+++ front-end/mapp/app/specs/DoctorListSpec.js
@@ -48,7 +48,7 @@ export default function(spec) {
     spec.it("Select user type and fill in account info", async function() {
       await spec.press("Welcome.Patient");
       await spec.pause(1500);
-      await spec.fillIn("SignIn.TextInput.Email", "notanemail");
+      await spec.fillIn("SignIn.TextInput.Email", "invalid@email.com");
       await spec.pause(1500);
       await spec.fillIn("SignIn.TextInput.Password", "invalid");
       await spec.pause(1500);
@@ -58,4 +58,67 @@ export default function(spec) {
       await spec.pause(2000);
     });
   });
+
+  ////////////////////////// Doctor SignIn /////////////////////////////
+
+  spec.describe("Sign in as a valid Doctor with valid password", function() {
+    spec.it("Select user type and fill in account info", async function() {
+      await spec.press("Welcome.Doctor");
+      await spec.pause(1500);
+      await spec.fillIn("SignIn.TextInput.Email", "test@gmail.com");
+      await spec.pause(1500);
+      await spec.fillIn("SignIn.TextInput.Password", "testing");
+      await spec.pause(1500);
+      await spec.press("SignIn.Button.SignIn");
+      await spec.pause(2000);
+      await spec.exists("PrescriptionCardComponent");
+      await spec.pause(2000);
+    });
+  });
+
+  spec.describe("Sign in as a valid Doctor with invalid password", function() {
+    spec.it("Select user type and fill in account info", async function() {
+      await spec.press("Welcome.Doctor");
+      await spec.pause(1500);
+      await spec.fillIn("SignIn.TextInput.Email", "test@gmail.com");
+      await spec.pause(1500);
+      await spec.fillIn("SignIn.TextInput.Password", "invalid");
+      await spec.pause(1500);
+      await spec.press("SignIn.Button.SignIn");
+      await spec.pause(2000);
+      await spec.exists("SignIn.TextInput.Email");
+      await spec.pause(2000);
+    });
+  });
+
+  spec.describe("Sign in as an invalid Doctor with valid password", function() {
+    spec.it("Select user type and fill in account info", async function() {
+      await spec.press("Welcome.Doctor");
+      await spec.pause(1500);
+      await spec.fillIn("SignIn.TextInput.Email", "PatInvalid@gmail.com");
+      await spec.pause(1500);
+      await spec.fillIn("SignIn.TextInput.Password", "testing");
+      await spec.pause(1500);
+      await spec.press("SignIn.Button.SignIn");
+      await spec.pause(2000);
+      await spec.exists("SignIn.TextInput.Email");
+      await spec.pause(2000);
+    });
+  });
+
+  spec.describe("Sign in as an invalid Doctor with invalid password", function() {
+    spec.it("Select user type and fill in account info", async function() {
+      await spec.press("Welcome.Doctor");
+      await spec.pause(1500);
+      await spec.fillIn("SignIn.TextInput.Email", "invalid@email.com");
+      await spec.pause(1500);
+      await spec.fillIn("SignIn.TextInput.Password", "invalid");
+      await spec.pause(1500);
+      await spec.press("SignIn.Button.SignIn");
+      await spec.pause(2000);
+      await spec.exists("SignIn.TextInput.Email");
+      await spec.pause(2000);
+    });
+  });
+  
 }
