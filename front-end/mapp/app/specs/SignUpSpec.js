ee0954bb545f86aab1a37ffb195c597da5d6631b
Sina Saleh <sinasaleh13@yahoo.com>
2018-11-04 19:59:10 -0800

    Sign Up Bad Inputs test

--- front-end/mapp/app/specs/SignUpSpec.js
+++ front-end/mapp/app/specs/SignUpSpec.js
@@ -2,7 +2,7 @@ export default function(spec) {
   spec.describe("Sign up as a patient", function() {
     spec.it("Select user type and fill in accoung info", async function() {
       await spec.press("Welcome.Patient");
-      await spec.pause(1500);
+      await spec.pause(500);
       await spec.press("SignIn.Button.SignUp");
       await spec.pause(500);
       await spec.fillIn("SignUp.TextInput.Email", "ablah@a.com");
@@ -12,8 +12,35 @@ export default function(spec) {
       await spec.fillIn("SignUp.TextInput.First Name", "sina");
       await spec.fillIn("SignUp.TextInput.Last Name", "saleh");
       await spec.press("SignUp.Button.SignUp");
+      await spec.pause(1000);
+    });
+  });
+  spec.describe("Sign up as a patient", function() {
+    spec.it("Only fill in email", async function() {
+      await spec.press("Welcome.Patient");
+      await spec.pause(500);
+      await spec.press("SignIn.Button.SignUp");
+      await spec.pause(500);
+      await spec.fillIn("SignUp.TextInput.Email", "ablah@a.com");
+      await spec.pause(500);
+      await spec.exists("SignUp.TextInput.Password");
+      await spec.pause(1000);
+    });
+  });
+  spec.describe("Sign up as a patient", function() {
+    spec.it("Only fill in email and password", async function() {
+      await spec.press("Welcome.Patient");
+      await spec.pause(500);
+      await spec.press("SignIn.Button.SignUp");
+      await spec.pause(500);
+      await spec.fillIn("SignUp.TextInput.Email", "ablah@a.com");
+      await spec.pause(500);
+      await spec.fillIn("SignUp.TextInput.Password", "ablahh");
+      await spec.fillIn("SignUp.TextInput.Confirm Password", "ablahh");
+      await spec.press("SignUp.Button.SignUp");
+      await spec.pause(500);
+      await spec.exists("SignUp.TextInput.Password");
       //await spec.pause(2000);
     });
   });
-
 }
